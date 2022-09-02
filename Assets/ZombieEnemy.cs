2420891d2ec8a0061685b2a75ac88a2747bdffa8
Wessl <lovewessman@gmail.com>
2022-09-02 20:46:45 +0200

    activate audio source over time

--- Assets/ZombieEnemy.cs
+++ Assets/ZombieEnemy.cs
@@ -7,6 +7,14 @@ public class ZombieEnemy : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private GameObject activateAfterGO;
+    private AudioSource activateAfterAS;
+    private float t = 0;
+
+    private void Start()
+    {
+        _audioSource.volume = 0f;
+    }
+
     private void OnEnable()
     {
         LiDARShooter.OnThresholdReached += Act;
@@ -20,21 +28,30 @@ public class ZombieEnemy : MonoBehaviour
     void Act()
     {
         _audioSource.Play();    // Zombie argharhh sound
-        LiDARShooter.OnThresholdReached -= Act;     // unsubscribe ourselves since we olnly want this occur once
+        LiDARShooter.OnThresholdReached -= Act;     // unsubscribe ourselves since we only want this occur once
 
         CreatePointsOnMesh();
         
         // Remove zombie from world right after...?
-        Destroy(gameObject);
+        // Destroy(gameObject);
         
         // activate 3d sound source somewhere...?
         activateAfterGO.SetActive(true);
+        activateAfterAS = activateAfterGO.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (activateAfterGO.activeSelf)
+        {
+            t += 0.25f * Time.deltaTime;
+            activateAfterAS.volume = Mathf.Lerp(0, 0.8f, t);
+        }
     }
 
     private void CreatePointsOnMesh()
     {
         DrawCircles drawCircles = GameObject.FindObjectOfType<DrawCircles>();
-
         
         var allMeshRenderers = gameObject.transform.GetComponentsInChildren<SkinnedMeshRenderer>();
         foreach (var meshRenderer in allMeshRenderers)
@@ -53,6 +70,5 @@ public class ZombieEnemy : MonoBehaviour
             }
             drawCircles.UploadCircleData(vertices.ToArray(), colors.ToArray());    
         }
-        
     }
 }
