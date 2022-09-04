20b22a706eedbfa736e4a5e9754e8a0c3292b96b
Wessl <lovewessman@gmail.com>
2022-09-04 22:58:14 +0200

    add the kiki music :)
    pretty cool!!

--- Assets/EdoMan.cs
+++ Assets/EdoMan.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 
 public class EdoMan : MonoBehaviour
 {
-    //[SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioSource _audioSource;
     [SerializeField] private GameObject activateAfterGO;
     [SerializeField] private float activeSec = 4f;
     private AudioSource activateAfterAS;
     private bool activated;
     private DrawCircles drawCircles;
+    public GameObject backWall;
     
     private float t = 0;
     Camera cam;
 
     private void Start()
     {
-        // _audioSource.volume = 0f;
+        _audioSource.volume = 0f;
         cam = Camera.main;
         drawCircles = GameObject.FindObjectOfType<DrawCircles>();
         activated = false;
-        GameObject.FindObjectOfType<LiDARShooter>().enemyHitAmountTriggerThreshold = 1000;
+        GameObject.FindObjectOfType<LiDARShooter>().enemyHitAmountTriggerThreshold = 1000; //arbitrary but works okay. 
     }
 
     private void OnEnable()
@@ -37,11 +38,14 @@ public class EdoMan : MonoBehaviour
     void Act()
     {
         Debug.Log("somefin happen");
-        //_audioSource.Play();    // PLay some sound
+        _audioSource.Play();    // PLay some sound
         LiDARShooter.OnThresholdReached -= Act;     // unsubscribe ourselves since we only want this occur once
         activated = true;
+        
+        // See Update() for all the cool shit that happens after activation
 
-
+     
+        
 
         // activate 3d sound source somewhere...?
         // activateAfterGO.SetActive(true);
@@ -52,8 +56,22 @@ public class EdoMan : MonoBehaviour
     }
 
     private void Update()
-    {// Make the points jiggle around a little
-        if ( activated ) drawCircles.JigglePoints();
+    {
+        if (activated)
+        {
+            t += 1 / activeSec * Time.deltaTime;
+            _audioSource.volume = Mathf.Lerp(0, 0.8f, t);
+            // Make the points jiggle around a little
+            drawCircles.JigglePoints();
+            // Have the back wall move down
+            backWall.transform.position = Vector3.Lerp(Vector3.zero, Vector3.down*4, t);
+            
+            // turn off activation...
+            if (t >= 0.98)
+            {
+                activated = false;
+            }
+        }
         
     }
 }
